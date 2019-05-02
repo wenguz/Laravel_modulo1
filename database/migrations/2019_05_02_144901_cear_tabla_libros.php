580eb168b4520d410b098a439b07f4cd43960a95
wenguz <wenguz1518@gmail.com>
2019-05-02 10:52:04 -0400

    Creacion migracion lilbros

---
+++ database/migrations/2019_05_02_144901_cear_tabla_libros.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CearTablaLibros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo',150);
+            $table->double('precio',7,2);
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
